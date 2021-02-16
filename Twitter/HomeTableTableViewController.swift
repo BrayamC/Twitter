3d3a17740425ac86b8dae870fea4d856209eee18
Brayam Corral Flores <brayamcorral@gmail.com>
2021-02-15 17:07:01 -0800

    Added pull down feature to refresh tweets

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -12,13 +12,19 @@ class HomeTableTableViewController: UITableViewController {
     
     var tweetArray = [NSDictionary]()
     var numberOfTweet: Int!
+    
+    let myRefreshControl = UIRefreshControl()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTweet()
+        
+        // pull down to refresh
+        myRefreshControl.addTarget(self, action:#selector(loadTweet), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
     }
 
-    func loadTweet(){
+    @objc func loadTweet(){
     
         
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
@@ -34,13 +40,12 @@ class HomeTableTableViewController: UITableViewController {
             
             // always make sure to reload data
             self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
             
         }, failure: { (Error) in
             print("Could not recieve tweets! oh no!!")
         })
-        
-        
-        
+ 
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
