5a61cbc91b5a040cafa581cdbd337f466a7cea96
Brayam Corral Flores <brayamcorral@gmail.com>
2021-02-18 18:15:08 -0800

    Added retweet and like buttons. Like button turns red if someone was liked

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -100,6 +100,8 @@ class HomeTableTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        
         return cell;
     }
     
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -14,6 +14,26 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+    }
+    @IBAction func retweet(_ sender: Any) {
+    }
+    
+    var favorited:Bool = false
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if (favorited) {
+            favButton.setImage(UIImage(named: "redHeart"), for: UIControl.State.normal)
+        }else{
+            favButton.setImage(UIImage(named: "emptyHeart"), for: UIControl.State.normal)
+        }
+        
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         tweetContent.sizeToFit()
