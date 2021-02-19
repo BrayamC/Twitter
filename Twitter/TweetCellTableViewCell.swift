14abbcb06d79558b3b4c4148f8be0497270c64fe
Brayam Corral Flores <brayamcorral@gmail.com>
2021-02-18 18:36:58 -0800

    Can like tweets. unliking doesn't work

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -17,19 +17,35 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var retweetButton: UIButton!
     @IBOutlet weak var favButton: UIButton!
     
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
     @IBAction func favoriteTweet(_ sender: Any) {
-    }
-    @IBAction func retweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        }
     }
     
-    var favorited:Bool = false
+    @IBAction func retweet(_ sender: Any) {
+    }
     
     func setFavorite(_ isFavorited:Bool){
         favorited = isFavorited
         if (favorited) {
             favButton.setImage(UIImage(named: "redHeart"), for: UIControl.State.normal)
         }else{
-            favButton.setImage(UIImage(named: "emptyHeart"), for: UIControl.State.normal)
+            favButton.setImage(UIImage(named: "coloredRetweet"), for: UIControl.State.normal)
         }
         
     }
