4744292f9f785d9e720fc8633550c83c8b87687e
Brayam Corral Flores <brayamcorral@gmail.com>
2021-02-21 17:48:09 -0800

    added swift file to second tab

---
+++ Twitter/profileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  profileViewController.swift
+//  Twitter
+//
+//  Created by Brayam Corral on 2/21/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class profileViewController: UIViewController {
+
+    @IBOutlet weak var temp: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        temp.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
