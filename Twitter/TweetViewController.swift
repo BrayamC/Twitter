265e870947c73ff4209faaf11f9eaa57df6d012f
Brayam Corral Flores <brayamcorral@gmail.com>
2021-02-21 16:50:36 -0800

    Added limit to amount of characters per tweet

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import RSKPlaceholderTextView
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var characterCountTextView: UILabel!
     @IBOutlet weak var tweetTextView: UITextView!
     @IBAction func cancel(_ sender: Any) {
         
@@ -29,10 +31,34 @@ class TweetViewController: UIViewController {
         }
     }
     
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+       // TODO: Check the proposed new text character count
+       // Allow or disallow the new text
+        
+        // Set the max character limit
+        let characterLimit = 280
+
+        // Construct what the new text would be if we allowed the user's latest edit
+        let newText = NSString(string: tweetTextView.text!).replacingCharacters(in: range, with: text)
+
+        // TODO: Update Character Count Label
+        let charText = String(newText.count)
+        self.characterCountTextView.text = "\(charText)/280"
+        
+        // The new text should be allowed? True/False
+        return newText.count <= characterLimit
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tweetTextView.becomeFirstResponder()
+        tweetTextView.delegate = self
+        
+        tweetTextView!.layer.borderWidth = 1
+        tweetTextView!.layer.borderColor = UIColor.black.cgColor
+        tweetTextView.layer.masksToBounds = true
+        tweetTextView.layer.cornerRadius = 10
+        
         // Do any additional setup after loading the view.
     }
     
